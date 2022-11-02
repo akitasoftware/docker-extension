ab2c8b13475484023677adfb16dc6dac0458f241
versilis <versilis@akitasoftware.com>
2022-11-01 20:14:47 -0500

    Move docker client hook

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,15 +1,5 @@
 import React from 'react';
-import Button from '@mui/material/Button';
-import { createDockerDesktopClient } from '@docker/extension-api-client';
-import { Stack, TextField, Typography } from '@mui/material';
-
-// Note: This line relies on Docker Desktop's presence as a host application.
-// If you're running this React app in a browser, it won't work properly.
-const client = createDockerDesktopClient();
-
-function useDockerDesktopClient() {
-  return client;
-}
 
 export function App() {
+  return <></>
 }

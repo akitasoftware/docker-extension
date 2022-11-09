00e72f9facb267ffeebc970d858aa1215780684a
versilis <versilis@akitasoftware.com>
2022-11-09 13:38:28 -0600

    Persist config changes to host file

--- vm/domain/failure/errors.go
+++ vm/domain/failure/errors.go
@@ -5,9 +5,17 @@ import (
 	"fmt"
 )
 
-var ErrInvalid = errors.New("invalid")
+var (
+	ErrInvalid  = errors.New("invalid")
+	ErrNotFound = errors.New("not found")
+)
 
 // Returns an ErrInvalid error along with the given message.
 func Invalidf(format string, a ...interface{}) error {
 	return fmt.Errorf("%w: %s", ErrInvalid, fmt.Sprintf(format, a...))
+}
+
+// Returns an ErrNotFound error along with the given message.
+func NotFoundf(format string, a ...interface{}) error {
+	return fmt.Errorf("%w: %s", ErrNotFound, fmt.Sprintf(format, a...))
 }
--- vm/infrastructure/repo/agent.go
+++ vm/infrastructure/repo/agent.go
@@ -2,27 +2,61 @@ package repo
 
 import (
 	"akita/domain/agent"
+	"akita/domain/failure"
+	"akita/infrastructure/datasource"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/spf13/afero"
 )
 
-// TODO: Persist the agent config to a file.
 type AgentRepository struct {
-	agentConfig *agent.Config
 }
 
-func NewAgentRepository() agent.Repository {
+func NewAgentRepository() *AgentRepository {
 	return &AgentRepository{}
 }
 
 func (a AgentRepository) GetConfig() (*agent.Config, error) {
-	return a.agentConfig, nil
+	data, err := afero.ReadFile(datasource.FileSystem, datasource.ConfigFilePath)
+	if err != nil {
+		if errors.Is(err, afero.ErrFileNotFound) {
+			return nil, failure.NotFoundf("agent config not found")
+		}
+		return nil, fmt.Errorf("failed to read agent config: %w", err)
+	}
+
+	var result *agent.Config
+
+	if err := json.Unmarshal(data, &result); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal agent config: %w", err)
+	}
+
+	return result, nil
 }
 
 func (a AgentRepository) CreateConfig(agentConfig *agent.Config) error {
-	a.agentConfig = agentConfig
+	data, err := json.Marshal(agentConfig)
+	if err != nil {
+		return fmt.Errorf("failed to marshal agent config: %w", err)
+	}
+
+	if err := afero.WriteFile(
+		datasource.FileSystem,
+		datasource.ConfigFilePath,
+		data,
+		datasource.OS_USER_RW,
+	); err != nil {
+		return fmt.Errorf("failed to write agent config: %w", err)
+	}
+
 	return nil
 }
 
 func (a AgentRepository) RemoveConfig() error {
-	a.agentConfig = nil
+	if err := datasource.FileSystem.Remove(datasource.ConfigFilePath); err != nil {
+		return fmt.Errorf("failed to remove agent config: %w", err)
+	}
+
 	return nil
 }

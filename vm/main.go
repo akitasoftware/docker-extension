b8cb5a9d96c7345a943b3203c6dc38e58e1c69d4
versilis <versilis@akitasoftware.com>
2022-11-09 10:28:13 -0600

    Remove boilerplate

--- vm/main.go
+++ vm/main.go
@@ -7,10 +7,8 @@ import (
 	"flag"
 	"log"
 	"net"
-	"net/http"
 	"os"
 
-	"github.com/labstack/echo"
 	"github.com/sirupsen/logrus"
 )
 
@@ -41,19 +39,9 @@ func main() {
 	}
 	router.Listener = ln
 
-	router.GET("/hello", hello)
-
 	log.Fatal(router.Start(startURL))
 }
 
 func listen(path string) (net.Listener, error) {
 	return net.Listen("unix", path)
-}
-
-func hello(ctx echo.Context) error {
-	return ctx.JSON(http.StatusOK, HTTPMessageBody{Message: "hello"})
-}
-
-type HTTPMessageBody struct {
-	Message string
 }

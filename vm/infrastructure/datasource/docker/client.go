7b57b964b9d12dd78aebd46958798fa406781390
versilis <versilis@akitasoftware.com>
2022-12-16 14:13:01 -0600

    Add docker client method to list containers

--- vm/infrastructure/datasource/docker/client.go
+++ vm/infrastructure/datasource/docker/client.go
@@ -1,6 +1,10 @@
 package docker
 
-import docker "github.com/docker/docker/client"
+import (
+	"context"
+	dockertypes "github.com/docker/docker/api/types"
+	docker "github.com/docker/docker/client"
+)
 
 type Client struct {
 	cli *docker.Client
@@ -13,4 +17,11 @@ func NewClient() (*Client, error) {
 	}
 
 	return &Client{cli: cli}, nil
+}
+
+func (c Client) ListContainers(
+	ctx context.Context,
+	opts dockertypes.ContainerListOptions,
+) ([]dockertypes.Container, error) {
+	return c.cli.ContainerList(ctx, opts)
 }

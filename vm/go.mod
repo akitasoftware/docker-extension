c45e41432a5654fc6865a7f01cd6461abaa87b06
versilis <versilis@akitasoftware.com>
2023-04-11 12:31:37 -0500

    Add methods to send demo server traffic

--- vm/go.mod
+++ vm/go.mod
@@ -16,10 +16,12 @@ require (
 
 require (
 	github.com/Microsoft/go-winio v0.6.0 // indirect
+	github.com/brianvoe/gofakeit/v6 v6.20.2 // indirect
 	github.com/docker/distribution v2.8.1+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/dukex/mixpanel v1.0.1 // indirect
+	github.com/go-faker/faker/v4 v4.1.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
 	github.com/golang/snappy v0.0.1 // indirect
@@ -45,13 +47,13 @@ require (
 	github.com/xdg-go/stringprep v1.0.3 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/crypto v0.1.0 // indirect
-	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
-	golang.org/x/net v0.1.0 // indirect
-	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
-	golang.org/x/sys v0.1.0 // indirect
-	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/net v0.6.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
-	golang.org/x/tools v0.1.12 // indirect
+	golang.org/x/tools v0.6.0 // indirect
 	gotest.tools/v3 v3.4.0 // indirect
 )
 
--- vm/infrastructure/datasource/demo-server.go
+++ vm/infrastructure/datasource/demo-server.go
@@ -2,16 +2,109 @@ package datasource
 
 import (
 	"fmt"
+	"github.com/brianvoe/gofakeit/v6"
 	"github.com/go-resty/resty/v2"
 )
 
-type DemoServer struct {
-	Client *resty.Client
-}
+var (
+	// Represents the possible tricks that can be sent to the demo server with their respective weights.
+	tricks map[string]float32
+	// Represents the possible breeds that can be sent to the demo server with their respective weights.
+	breeds map[string]float32
+)
+
+type (
+	DemoServer interface {
+		// Send a random breed request to the demo server.
+		GetBreed() error
+		// Send a random trick request to the demo server.
+		PostTrick() error
+	}
+	demoServerImpl struct {
+		client *resty.Client
+	}
+)
 
 // Creates a new demo server client.
-func ProvideDemoServer(port int) *DemoServer {
-	return &DemoServer{
-		Client: resty.New().SetBaseURL(fmt.Sprintf("http://demo-server:%d", port)),
+func ProvideDemoServer(port int) DemoServer {
+	return &demoServerImpl{
+		client: resty.New().SetBaseURL(fmt.Sprintf("http://demo-server:%d", port)),
+	}
+}
+
+func (d demoServerImpl) GetBreed() error {
+	var breedParams []interface{}
+	var weights []float32
+	for breed, weight := range breeds {
+		breedParams = append(breedParams, breed)
+		weights = append(weights, weight)
+	}
+
+	pick, err := gofakeit.New(0).Weighted(breedParams, weights)
+	if err != nil {
+		return fmt.Errorf("failed to create breed faker: %w", err)
+	}
+
+	_, err = d.client.R().Get(fmt.Sprintf("/v1/breed/%s", pick))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d demoServerImpl) PostTrick() error {
+	var trickParams []interface{}
+	var weights []float32
+	for trick, weight := range tricks {
+		trickParams = append(trickParams, trick)
+		weights = append(weights, weight)
+	}
+
+	pickedTrick, err := gofakeit.New(0).Weighted(trickParams, weights)
+	if err != nil {
+		return fmt.Errorf("failed to create trick faker: %w", err)
+	}
+
+	body := fmt.Sprintf(
+		`{"trick":"%s", owner: {id: %s, name: %s, address: %s}}`,
+		pickedTrick,
+		gofakeit.UUID(),
+		gofakeit.Name(),
+		gofakeit.Address().Address,
+	)
+	url := fmt.Sprintf("/v1/trick/%s", pickedTrick)
+
+	_, err = d.client.R().SetBody(body).Post(url)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func init() {
+	tricks = map[string]float32{
+		"rollover":  0.3,
+		"fetch":     0.1,
+		"shake":     0.1,
+		"jump":      0.1,
+		"spin":      0.1,
+		"beg":       0.1,
+		"play-dead": 0.1,
+		"sit":       0.1,
+	}
+
+	breeds = map[string]float32{
+		// Will return 200s
+		"labrador": 0.2,
+		"akita":    0.2,
+		"poodle":   0.1,
+		"husky":    0.1,
+		// Will return 404s
+		gofakeit.Dog(): 0.1,
+		gofakeit.Dog(): 0.1,
+		gofakeit.Dog(): 0.1,
+		gofakeit.Dog(): 0.1,
 	}
 }
